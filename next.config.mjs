5bc765b0266f61c6803407b4ed4542ea32b162d4
Sumit <sumitgarg380@gmail.com>
2024-03-15 00:19:04 +0530

    Added Background and Navbar

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domain: ['assets.aceternity.com'],
+    }
+};
 
 export default nextConfig;
